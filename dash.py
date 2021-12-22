e754eccac1d893b933cf68bcf13758dd18a2205d
AxiomME <96500411+AxiomME@users.noreply.github.com>
2021-12-22 15:01:21 +0100

    Add files via upload

--- dash.py
+++ dash.py
@@ -388,7 +388,7 @@ def main():
 				fig1=count2(quest.iloc[i]['variable_x'],quest.iloc[i]['variable_y'],\
 				df,legendtitle=quest.iloc[i]['legendtitle'],xaxis=quest.iloc[i]['xtitle'])
 				fig1.update_layout(title_text=quest.iloc[i]['legendtitle'],font=dict(size=12),showlegend=True)	
-				fig1.update_layout(legend_title_side='top',legend_font_size=15,legend_title_font_size=16,showlegend=True,title=None)
+				fig1.update_layout(legend_title_side='top',legend_font_size=14,legend_title_font_size=14,showlegend=True,title=None)
 				col1.plotly_chart(fig1,use_container_width=True)
 						
 				fig2=pourcent2(quest.iloc[i]['variable_x'],quest.iloc[i]['variable_y'],\
